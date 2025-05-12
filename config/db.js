8cf299eb3b261d93a2d9ca0dd3c54508f575a018
akuma19 <kellyhandy31@gmail.com>
2025-05-12 23:15:09 +0100

    done

--- config/db.js
+++ config/db.js
@@ -6,9 +6,9 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, TEST_DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
 
-if (!DB_HOST || !DB_PASSWORD || !DB_NAME || !DB_USER || !DB_PORT || !TEST_DB_NAME) {
+if (!DB_HOST || !DB_PASSWORD || !DB_NAME || !DB_USER || !DB_PORT) {
   logger.error(
     "Database environment variables are missing! Check your .env file."
   );
